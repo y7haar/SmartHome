9078a8ca81db97e7bdb5d517dab511eec23aa7bc
Yannic Siebenhaar <yannic.siebenhaar@hof-university.de>
2016-05-09 11:30:20 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yannic on 09.05.16.
+ */
+
+var app = angular.module('MainApp', ['ngMaterial']);
+
+var test = {lol: "123",
+    test:42};
+
+localStorage.setObject("test", test);
+
+console.log(localStorage.getObject("test"));
---
+++ app/app.route.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yannic on 09.05.16.
+ */
