fa6f169eb7a82eb81bf85f4bc2613053315210f6
Alex K <alex-kern@online.de>
2016-06-22 19:34:26 +0200

    -changes: Log Module

--- app/controllers/modules/logController.js
+++ app/controllers/modules/logController.js
@@ -5,7 +5,19 @@
 
 mainApp.controller("logCtrl", function ($scope) {
 
+    $scope.logMsgs = ["[Wohnzimmer][Licht1][Maria] angeschaltet um [Datum]",
+        "[Wohnzimmer][Fenster][Maria] geöffnet um [Datum]",
+        "[Wohnzimmer][Licht1][Maria] ausgemacht um [Datum]",
+        "[Wohnzimmer][Licht2][Maria] angemacht um [Datum]"];
 
     $scope.users = Storage.getInstance().getUsers();
+    $scope.modules = {
+
+    };
+
+
+
+
+    console.dir($scope.users);
 
 });
